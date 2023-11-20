0d65d7bc22f89b56857705d64e89bcb1a49ba828
Amab <miynaoje@outlook.com>
2023-11-21 00:36:34 +0100

    Upated

--- encrypt.py
+++ encrypt.py
@@ -36,11 +36,49 @@ def decrypt(ciphertext, private_key):
     )
     return plaintext.decode()
 
+# save the key function
+def save_key(key, filename):
+    with open(filename, 'wb') as key_file:
+        key_file.write(key)
+
+# load the key function
+def load_key(filename):
+    with open(filename, 'rb') as key_file:
+        key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+        )
+    return key
+
+
+
+
+
+
 
 private_key, public_key = key_pair()
+
+# private key saved
+save_key(private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+), 'private_key.pem')
+
+# public key saved
+save_key(public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+), 'public_key.pem')
+
+# the following are used to load the keys from respective files
+# loaded_private_key = load_key('private_key.pem')
+# loaded_public_key = load_key('public_key.pem')
+
+
 
 message = input("Enter message to encryt: ")
 ciphertext = encrypt(message, public_key)
 decrypted_message = decrypt(ciphertext, private_key)
 print(f"Encrypted message: {ciphertext}")
-print(f"Decrypted message: {decrypted_message}")
+#print(f"Decrypted message: {decrypted_message}")
